6d35a3f4817f8ff4f9bab2ea3ce8753625d047c8
sebapastore <sebastianerp@gmail.com>
2022-02-08 15:08:56 -0300

    Login Test and some test setup

--- tests/Feature/Login/LoginTest.php
+++ tests/Feature/Login/LoginTest.php
@@ -2,15 +2,31 @@
 
 namespace Tests\Feature\Login;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
     /** @test */
-    public function login_shows_email_and_password_inputs()
+    public function login_page_is_rendering()
+    {
+        $this->get('/login')->assertSuccessful();
+    }
+
+    /** @test */
+    public function user_can_login()
     {
         $this->withoutExceptionHandling();
 
-        $this->get('/login')->assertSuccessful();
+        $user = User::factory()->create();
+
+        $data = ['email' => $user->email, 'password' => 'password'];
+
+        $response = $this->post('/login', $data);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect('/dashboard');
+
     }
 }
