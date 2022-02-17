3d279a84f835718fd4dc74c039d930417238c2b4
Luis Leguizamon <lleguizamonl@gmail.com>
2022-02-17 19:42:46 -0300

    Create domain: Adjust title

--- resources/js/Pages/Domains/CreateForm.vue
+++ resources/js/Pages/Domains/CreateForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <app-layout title="Crear Usuario">
+    <app-layout title="Crear Dominio">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Crear Dominio
