a632eaceeaa4a364d18b04b88e042d1e63a33e7b
sebapastore <sebastianerp@gmail.com>
2022-03-17 19:01:26 -0300

    Add DomianFactory

---
+++ database/factories/DomainFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Domain;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Laravel\Jetstream\Features;
+
+class DomainFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Domain::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->domainName(),
+        ];
+    }
+}
