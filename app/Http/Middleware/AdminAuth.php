22d15515db394cbf0df79856ef2b0068a5d9815c
sebapastore <sebastianerp@gmail.com>
2022-03-17 16:24:25 -0300

    Admin Middleware

---
+++ app/Http/Middleware/AdminAuth.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if($user->role != User::ROLE_ADMIN) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return $next($request);
+    }
+}
